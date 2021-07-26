b56a3eda54f424977b8824a61608835d92ee45f0
[banjman81] <[benjamin.man97@gmail.com]>
2021-07-26 14:54:08 -0500

    v1.1

--- main.js
+++ main.js
@@ -9,34 +9,43 @@ const counter = $('.counter');
 const kickNums = $('.kickdrum-number input[type="radio"]');
 const snareNums = $('.snaredrum-number input[type="radio"]');
 const hiNums = $('.hihat-number input[type="radio"]');
+const metronome = $('#metronome')
 
 let count = 1
 let kickDrum = 0
 let snareDrum = 0
 let hiHat = 0
+let metronomeCheck = false
 // This function is called every 600ms
 function update() {
-
     // Play the 'tick' sound
     if(count <4){
-        tick.play();
+        if(metronomeCheck === true){
+            tick.play();
+        }
         counter.text(`${count}`)
         soundPlayer(count)
         count++
     }
-
     // Play the 'tock' sound
     else if (count === 4){
-        tock.play()
+        if(metronomeCheck === true){
+            tock.play()
+        }
         counter.text(`${count}`)
         soundPlayer(count)
         count = 1
         // console.log('tock')
     }
-    
+    let arrayInst = []
     for(let inst of instSelect){
         if(inst.checked){
             if(inst.value === "kickDrum"){
+                let match = false
+                match = arrayCheck(inst.value)
+                if(match === false){
+                    arrayInst.push(inst.value)
+                }
                 for(let kick of kickNums){
                     if(kick.checked){
                         kickDrum = Number(kick.value)
@@ -44,6 +53,11 @@ function update() {
                 }
             }
             if(inst.value === "snareDrum"){
+                let match = false
+                match = arrayCheck(inst.value)
+                if(match === false){
+                    arrayInst.push(inst.value)
+                }
                 for(let snare of snareNums){
                     if(snare.checked){
                         snareDrum = Number(snare.value)
@@ -51,15 +65,43 @@ function update() {
                 }
             }
             if(inst.value === "hiHat"){
+                let match = false
+                match = arrayCheck(inst.value)
+                if(match === false){
+                    arrayInst.push(inst.value)
+                }
                 for(let hi of hiNums){
                     if(hi.checked){
                         hiHat = Number(hi.value)
                     }
                 }
             }
+
+            function arrayCheck(val){
+                for( list of arrayInst){
+                    if(list === val){
+                        return true
+                    }
+                }
+                return false
+            }
+
         }
     }
+    // check for unckecks
+    if(jQuery.inArray('kickDrum', arrayInst) == -1){
+        kickDrum = 0
+    }
+    if(jQuery.inArray('snareDrum', arrayInst) == -1){
+        snareDrum = 0
+    }
+    if(jQuery.inArray('hiHat', arrayInst) == -1){
+        hiHat = 0
+    }
 }
+metronome.on('click', function(){
+    metronomeCheck = !metronomeCheck
+})
 
 function soundPlayer(n){
     if(kickDrum === n){
